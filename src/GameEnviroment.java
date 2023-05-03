5bb2164e76b11257d3a5c195962a9ffb906631aa
John Chan <cch235@uclive.ac.nz>
2023-05-03 17:02:43 +1200

    pu

--- src/GameEnviroment.java
+++ src/GameEnviroment.java
@@ -1,4 +1,8 @@
+import java.util.*;
+
 
 public class GameEnviroment {
+	ArrayList<Athlete> team = new ArrayList<Athlete>(4);
+	
 
 }
--- src/Item.java
+++ src/Item.java
@@ -4,7 +4,6 @@ public class Item implements Purchasable {
 	private enum TYPE {
 		//tbd
 		//test
-		
 	}
 	
 	private int price;
