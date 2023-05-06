1bcca1c6ba98a4a2e00b883f5cf72a0dcd2b686f
John Chan <cch235@uclive.ac.nz>
2023-05-06 15:10:20 +1200

    created team class

---
+++ src/Cores/Team.java
@@ -0,0 +1,17 @@
+package Cores;
+
+import java.util.ArrayList;
+/**
+ * 
+ * @author chunyuenchan
+ * Take four athletes and store it in a array list
+ * TBD
+ */
+public class Team {
+	private ArrayList<Athlete> team = new ArrayList<Athlete>(4);
+	//name of team set by player
+	private String teamName;
+	//season length measure in week
+	
+
+}
