f58cdc1f0c84755d9fa662924b569d23c2d9fef2
John Chan <cch235@uclive.ac.nz>
2023-05-06 17:21:55 +1200

    javadocs

--- src/Cores/GameEnviroment.java
+++ src/Cores/GameEnviroment.java
@@ -1,12 +1,23 @@
 package Cores;
 import java.util.*;
 
-
+/**
+ * 
+ * Class that store game's data and manage the application  
+ *
+ */
 public class GameEnviroment {
-	//team list
+	
+	// length of the game
 	private int seasonLength;
+	
+	// current game progress, measure in week
 	private int currenytWeek;
+	
+	// score obtained by player 
 	private int score = 0;
+	
+	// money obtained by player
 	private int money = 0;
 	
 	
--- src/Cores/Item.java
+++ src/Cores/Item.java
@@ -1,20 +1,36 @@
 package Cores;
+
+/**
+ * 
+ * Class that describe behaviors object Item 
+ *
+ */
 public class Item implements Purchasable {
 	
+	/**
+	 * Indicates the type of item
+	 *
+	 */
 	private enum TYPE {
 		WEIGHT,
 		FOOD,
 		MEDICINE
 	}
 	
+	// price of the item
 	private int price;
+	
+	// item's worth 
 	private int worth;
 	
 
-	@Override
+	/**
+	 * get the price of item
+	 * 
+	 * @return price of the item 
+	 */
 	public int getPrice() {
-		// TODO Auto-generated method stub
-		return 0;
+		return this.price;
 	}
 
 	@Override
