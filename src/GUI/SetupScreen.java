940625d3f903baae81c48b83cdb3759839eee6f9
Riley Lindsay <rli107@uclive.ac.nz>
2023-05-16 13:44:46 +1200

    setup screen GUI

--- src/GUI/SetupScreen.java
+++ src/GUI/SetupScreen.java
@@ -16,7 +16,7 @@ import java.util.ArrayList;
 
 public class SetupScreen {
 
-	private JFrame frmGameenvironmentSetupScreen;
+	private JFrame frame;
 	private JTextField nameTextField;
 	private ArrayList<Athlete> startingAthletes = new ArrayList<>();
 	private GameEnvironment manager;
@@ -29,7 +29,7 @@ public class SetupScreen {
 //			public void run() {
 //				try {
 //					SetupScreen window = new SetupScreen();
-//					window.frmGameenvironmentSetupScreen.setVisible(true);
+//					window.frame.setVisible(true);
 //				} catch (Exception e) {
 //					e.printStackTrace();
 //				}
@@ -50,44 +50,44 @@ public class SetupScreen {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		frmGameenvironmentSetupScreen = new JFrame();
-		frmGameenvironmentSetupScreen.setTitle("GameEnvironment Setup Screen");
-		frmGameenvironmentSetupScreen.setBounds(100, 100, 450, 300);
-		frmGameenvironmentSetupScreen.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frmGameenvironmentSetupScreen.getContentPane().setLayout(null);
+		frame = new JFrame();
+		frame.setTitle("GameEnvironment Setup Screen");
+		frame.setBounds(100, 100, 450, 300);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.getContentPane().setLayout(null);
 		
 		JLabel lblWelcome = new JLabel("Welcome to the Game!");
 		lblWelcome.setBounds(10, 11, 118, 14);
-		frmGameenvironmentSetupScreen.getContentPane().add(lblWelcome);
+		frame.getContentPane().add(lblWelcome);
 		
 		JLabel lblName = new JLabel("What is the name of your team?");
 		lblName.setBounds(10, 33, 164, 14);
-		frmGameenvironmentSetupScreen.getContentPane().add(lblName);
+		frame.getContentPane().add(lblName);
 		
 		JLabel lblLength = new JLabel("Select your season length");
 		lblLength.setBounds(10, 58, 164, 14);
-		frmGameenvironmentSetupScreen.getContentPane().add(lblLength);
+		frame.getContentPane().add(lblLength);
 		
 		JLabel lblAthletes = new JLabel("Select your starting Athletes");
 		lblAthletes.setBounds(10, 83, 164, 14);
-		frmGameenvironmentSetupScreen.getContentPane().add(lblAthletes);
+		frame.getContentPane().add(lblAthletes);
 		
 		nameTextField = new JTextField();
 		nameTextField.setBounds(255, 30, 129, 20);
-		frmGameenvironmentSetupScreen.getContentPane().add(nameTextField);
+		frame.getContentPane().add(nameTextField);
 		nameTextField.setColumns(10);
 		
 		JSlider lengthSlider = new JSlider();
 		lengthSlider.setMaximum(15);
 		lengthSlider.setMinimum(5);
 		lengthSlider.setBounds(184, 58, 200, 26);
-		frmGameenvironmentSetupScreen.getContentPane().add(lengthSlider);
+		frame.getContentPane().add(lengthSlider);
 		
 		DefaultListModel<Athlete> athleteListModel = new DefaultListModel<Athlete>();
 		athleteListModel.addAll(startingAthletes);
 		JList<Athlete> athleteList = new JList<Athlete>(athleteListModel);
 		athleteList.setBounds(20, 171, 362, -62);
-		frmGameenvironmentSetupScreen.getContentPane().add(athleteList);
+		frame.getContentPane().add(athleteList);
 		
 		athleteList.getSelectedValue();
 	}
