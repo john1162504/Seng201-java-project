7ae2cc9d824b9fe65f025179218443fce5663624
John Chan <cch235@uclive.ac.nz>
2023-05-20 16:45:43 +1200

    GameEnvironmentUi javadoc

--- src/UI/GameEnvironmentUi.java
+++ src/UI/GameEnvironmentUi.java
@@ -2,18 +2,43 @@ package UI;
 
 import Cores.GameEnvironment;
 
+/**
+ *User interface's interface  
+ *
+ */
 public interface GameEnvironmentUi {
-	
-    String NAME_REGEX = "[a-zA-Z]{3,}";
+	//Name regex use to test name validity
+    final static String NAME_REGEX = "[a-zA-Z]{3,15}";
     
+    //Name requirements 
+    final static String NAME_REQUIRMENTS = "Your name must only contain letters and have 3 to 15 charaters";
+    
+    /**
+     * Initialises this UI and setup the given game 
+     * @param game The game this interface interact with
+     */
     void setup(GameEnvironment game);
     
+    /**
+     * This method is called once setup is finished and allowing player move on to the main game
+     */
     void start();
     
+    /**
+     * Confirms if player really wants to quit
+     * 
+     * @return true if player confirm to quit, false otherwise
+     */
     boolean confirmQuit();
     
+    /**
+     * quit this application
+     */
     void quit();
     
-    
+    /**
+     * Report errors to the player
+     * @param error The error
+     */
     void showError(String error);
 }
