e7334f0e1fec4d2038ad4a213760a0c1867d6447
Riley Lindsay <rli107@uclive.ac.nz>
2023-05-21 20:24:04 +1200

    test

---
+++ src/Test/AthleteTest.java
@@ -0,0 +1,15 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import Cores.Athlete;
+
+class AthleteTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
